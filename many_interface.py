f3c0cdcee2a6878b5307a8ef69c586f5d1e4ecfd
SNFowler <stephen.nicholas.fowler@gmail.com>
2025-02-03 20:50:09 +1000

    Simplified code slightly

--- many_interface.py
+++ many_interface.py
@@ -1,12 +1,32 @@
 import serial
 import serial.tools.list_ports
 import os
+import json
+
+"""
+@TODO write a generic settings file. Apply.
+@TODO think about Send/Recieve. Do these need to be 2 functions or could the SCPI server code handle converting send/recieve commands and both of them be handled by the same method in this class? 
+@TODO load and apply the settings file.
+
+Ability to give windfreaks "nicknames" was removed for now, as it added a lot of code. 
+"""
 
 class windfreak_manager():
-    def __init__(self):
+    def __init__(self, config_path = "config/default.json"):
+        # load default settings
+        self.config_path = config_path    
+        self.config = None
         self.windfreaks = {}
-        self._refresh_wf_list()      
+        
+        # load settings
+        self.load_config()
+        
+        # Windfreak serials are indexed by static device ID (1182)
+        self._refresh_wf_list()
+
+        # apply settings   
         self.display_windfreaks()
+
 
         # things to summarise
         # Name, command
@@ -14,8 +34,52 @@ class windfreak_manager():
 
         }
 
+    def send(self, device_id, str_command):
+        try:
+            #send command to device, return the response (appears to usually just be a 1 or 0)
+            ser = self.windfreaks[device_id]
+            success_code = ser.write(user_input.encode())
+
+        except:
+            raise Exception(f"Invalid command: {device_id} -'{str_command}'")
+    
+    def recieve(self, device_id, str_command):
+            self.send(device_id, str_command)
+            # get the returned response as a single string.
+            ser = self.windfreaks[device_id]
+            response = ""
+            current_line = ser.readline().decode()
+            #Check for multiple return lines and add them to the response.
+            if current_line:
+                response += current_line
+                current_line = ser.readline().decode()
+
+            return response
+
+    def recall(self, custompath=None):
+        # loads the config file specified by the path. If no path is provided, the current previously used configpath is used.
+        if custompath: self.configpath = custompath
+        with open(self.configpath, 'r') as file:
+            self.config = json.load(file)
+
+    def store(self, custompath=None):
+        # saves the current config to custompath. If no path is provided, the current config file is overriden
+        if custompath: self.configpath = custompath
+        with open(self.configpath, "w") as outfile:
+            outfile.write(self.config)
+    
+    def load_config(self):
+        assert self.config_path
+        try:
+            with open(self.config_path, 'r') as file:
+                config = json.load(file)
+
+        except Exception as e:
+            raise Exception("Loading config failed:") from e
+        return config
+
     def _refresh_wf_list(self):
-        # Gets the currently connected windfreaks
+        # gets the currently connected windfreaks
         for port in serial.tools.list_ports.comports():
             # name of serial port
             name_ACM = port.device
@@ -28,69 +92,45 @@ class windfreak_manager():
             device_id = ser.readline().decode()[:-1]
             self.windfreaks[device_id] = ser
 
-    def display_windfreaks(self):
-        print("Current ports:")
-        for id in sorted(self.windfreaks.keys()):
-            print(f"{id} \t (via {self.windfreaks[id].port})")
-    
-    def send(self, device_id, str_command):
-        try:
-            #send command to device, return the response
-            ser = self.windfreaks[device_id]
-            return ser.write(user_input.encode()) 
-
-        except:
-            raise Exception(f"Invalid command: {device_id} -'{str_command}'")
-    
-    def recieve(self, device_id):
-            # get the returned response as a single string.
-            ser = self.windfreaks[device_id]
-            response = ""
-            current_line = ser.readline().decode()
-            #Check for multiple return lines and add them to the response.
-            if current_line:
-                response += current_line
-                current_line = ser.readline().decode()
-
-            return response
-
-
-
-    def command(self, device_id, str_command):
-        #combin
-        self.send(device_id, str_command)
-        return self.recieve(device_id)
+            # if the windfreak was given a shorter name in settings, also add an entry for that shorter  
     
     def close_all(self):
         #closes everything
         for id in self.windfreaks.keys():
             self.windfreaks[id].close()
+
+    def display_windfreaks(self):
+        print("Current ports:")
+        for id in sorted(self.windfreaks.keys()):
+            print(f"{id} \t (via {self.windfreaks[id].port})")
     
-    def summary_status(self):
-        # Should get a summary from the stored settings
-        pass
+    def status(self, verbose=False):
+        for id in sorted(self.windfreaks.keys()):
+            ser = self.windfreaks[id]
+            # send a request to the windfreak for current status
+            ser.write("?".encode())
+            status = ""
+            while True:
+                # continue reading in until entire status is done.
+                current = ser.readline().decode()
 
-    def save_settings_json():
-        pass
-    
-    def load_settings_json():
-        pass
+                # add recently read line to status.
+                status += current
+                if not current:
+                    break
 
+if __name__ == "__main__":        
+    manager = windfreak_manager()
 
-        
+    # Start a loop
+    while True:
+        id          = input("Control which windfreak?")
+        user_input  = input("Enter a string to send to the serial port (or 'q' to quit): ")
 
-        
-manager = windfreak_manager()
+        if user_input.lower() == 'q':
+            print("Exiting")
+            break  # Exit the loop
 
-# Start a loop
-while True:
-    id          = input("Control which windfreak?")
-    user_input  = input("Enter a string to send to the serial port (or 'q' to quit): ")
-
-    if user_input.lower() == 'q':
-        print("Exiting")
-        break  # Exit the loop
-
-    print(manager.command(id, user_input))
+        print(manager.recieve(id, user_input))
 
    
